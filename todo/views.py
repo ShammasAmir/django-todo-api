4e4e564a153300fdacc4f8c767cff2e92e71c7f2
ShammasAmir <amirshammas@gmail.com>
2023-08-11 12:29:35 +0330

    Create serializer to get the list of todos.

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def all_todos(request: Request):
+    todos = Todo.objects.all()
+    todo_serializer = TodoSerializer(todos, many=True)
+    return Response(todo_serializer.data, status.HTTP_200_OK)
