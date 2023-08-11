2c5f57d84f63b7f0b100ec2244083e2ee27da6aa
ShammasAmir <amirshammas@gmail.com>
2023-08-11 11:20:10 +0330

    Display list of todos as json.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page)
+    # path('', views.index_page)
+    path('', views.todo_json)
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from todo.models import Todo
+from django.http import HttpRequest, JsonResponse
 
 def index_page(request):
     context = {
         'todos': Todo.objects.all()
     }
     return render(request, 'home/index.html', context)
+
+def todo_json(request: HttpRequest):
+    todos = list(Todo.objects.all().values('title', 'is_done'))
+    return JsonResponse({'todos': todos})
