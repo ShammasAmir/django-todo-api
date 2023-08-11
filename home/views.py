fd58ba0d4a380b38b3805bb0e48934bc73fb8883
ShammasAmir <amirshammas@gmail.com>
2023-08-11 03:51:12 +0330

    Display list of todos in index page.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from todo.models import Todo
 
 def index_page(request):
-    context = {}
+    context = {
+        'todos': Todo.objects.all()
+    }
     return render(request, 'home/index.html', context)
