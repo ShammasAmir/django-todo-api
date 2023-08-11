9269ad6299baa9042966cb582229ec6caab0da2c
ShammasAmir <amirshammas@gmail.com>
2023-08-11 13:12:53 +0330

    Create POST method to add a new todo.

--- todo/views.py
+++ todo/views.py
@@ -6,8 +6,21 @@ from .serializers import TodoSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+# @api_view(['GET'])
+# def all_todos(request: Request):
+#     todos = Todo.objects.all()
+#     todo_serializer = TodoSerializer(todos, many=True)
+#     return Response(todo_serializer.data, status.HTTP_200_OK)
+
+@api_view(['GET', 'POST'])
 def all_todos(request: Request):
-    todos = Todo.objects.all()
-    todo_serializer = TodoSerializer(todos, many=True)
-    return Response(todo_serializer.data, status.HTTP_200_OK)
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        todo_serializer = TodoSerializer(todos, many=True)
+        return Response(todo_serializer.data, status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+    return Response(None, status.HTTP_400_BAD_REQUEST)
